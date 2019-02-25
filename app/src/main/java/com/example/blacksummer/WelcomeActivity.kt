e4fe364484f5d7ef73142a24da51abca8c63dc05
Alistair <AlistairGrout@users.noreply.github.com>
2019-02-25 21:22:44 +0100

    Ressources
    Welcome Activity + ressources + font + values (not finished)

---
+++ app/src/main/java/com/example/blacksummer/WelcomeActivity.kt
@@ -0,0 +1,18 @@
+package com.example.blacksummer
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class WelcomeActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
